1d65c595c7d0ec7af621ee7792efb8a16c309b54
George McNally <geomcn11@gmail.com>
2018-11-07 13:05:44 +0000

    Delete and Destriy added

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -35,4 +35,19 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  
+  def show
+  	@portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+  	@portfolio_item = Portfolio.find(params[:id])
+
+  	@portfolio_item.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+      format.json { head :no_content }
+    end
+  end
 end
