eab4b031bfc71d0e33827e6209c7b926ad9bdd7a
George McNally <geomcn11@gmail.com>
2018-11-12 03:07:58 +0000

    updates concerns and Technologies

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,17 +1,17 @@
 class Portfolio < ApplicationRecord
-
+	has_many :technologies
+	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
 	def self.angular
 		where(subtitle: 'Angular')
-		
 	end
 
 	after_initialize :set_defaults
 
 	def set_defaults
-		self.main_image ||= "https://via.placeholder.com/600x400"
-		self.thumb_image ||= "https://via.placeholder.com/350x200"
+		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height: '300', width: '200')
 	end
 
 end
