a1c0451a0ba3bdf6a559a7986b0a06180a7e3c68
George McNally <geomcn11@gmail.com>
2018-11-26 13:16:17 +0000

    added petergate and edited who can edit, create and delete items.

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -2,16 +2,16 @@ module CurrentUserConcern
 
 extend ActiveSupport::Concern
 
-
-
 	def current_user
 	  super || 	guest_user
 	end
 
 	def guest_user
-		OpenStruct.new(name: "Guest User", 
-	  						   first_name: "Guest", 
-	  						   last_name: "User", 
-	  						   email: "guestuser@gmail.com")
+		guest = GuestUser.new
+		guest.name = "Guest User"
+		guest.first_name = "Guest"
+		guest.last_name = "User"
+		guest.email = "guest@example.com"
+		guest
 	end
 end
