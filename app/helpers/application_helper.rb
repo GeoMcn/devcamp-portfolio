6d83aafd734841552be751b2a6f8d9b99c53a8a4
George McNally <geomcn11@gmail.com>
2018-11-27 21:55:43 +0000

    refactored nav link and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,42 @@ module ApplicationHelper
     	content_tag(:p, greeting, class: "source-greeting" )
       end
     end
+
+    def nav_items
+      [
+        {
+          url: root_path,
+          title: 'home'
+        },
+        {
+          url: about_path,
+          title: 'about'
+        },
+        {
+          url: contact_path,
+          title: 'contact'
+        },
+        {
+          url: portfolios_path,
+          title: 'portfolio'
+        },
+        {
+          url: blogs_path,
+          title: 'blog'
+        },
+      ]
+    end
+
+    def nav_helper style, tag_type
+
+      nav_links = ''
+      nav_items.each do |item|
+        nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+      end
+       nav_links.html_safe
+    end
+
+    def active? path
+      "active" if current_page? path
+    end
 end
