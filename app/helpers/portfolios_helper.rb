0afc54c903fb3b143b3d156b0939e1fd7c30a422
George McNally <geomcn11@gmail.com>
2018-12-03 09:46:15 +0000

    implemented edit button for portfolio

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,18 +1,16 @@
 module PortfoliosHelper
 
 	def image_generator(height:, width:)
-
-			"https://via.placeholder.com/#{height}x#{width}"
+		"https://placeholder.com/#{height}x#{width}"
 	end
 
 	def portfolio_img img, type
-		
-		if img.model.main_image? || img.model.thumb_image?
-			img
-		elsif type == 'thumb'
-			image_generator(height: '350', width: '200')
-		else 
-			image_generator(height: '600', width: '500')
-		end
-    end
+		if img.model.main_image? || img.model.thumb_image
+			img.to_s
+		elsif type == 'thumb'	
+			image_generator(height: '350', width: '200').to_s
+		elsif type == 'main'
+			image_generator(height: '600', width: '400').to_s
+		end			
+	end
 end
